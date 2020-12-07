25c67f396e15af8f9068cba21a2fe30d069de5f5
Edrson <ederson.ramirez87@gmail.com>
2020-12-07 01:06:35 -0600

    se agrega index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// importando el marco del nodo 
+var express = require ('express'); 
+ 
+var app = express ();
+// Responde con "hola mundo" a las solicitudes que llegan a nuestra raíz "/" 
+app.get ('/', function (req, res) { 
+res.send ('hola mundo'); 
+});
+// escucha el puerto 3000 por defecto 
+app.listen (process.env.PORT || 3000); 
+ 
+module.exports = aplicación;
