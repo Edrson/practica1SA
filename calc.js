12b127e80c88afbc632c39de7198633a3b508eb8
Edrson <ederson.ramirez87@gmail.com>
2020-12-07 05:31:59 -0600

    se agrega pagina para pruebas

--- calc.js
+++ calc.js
@@ -1,3 +1,10 @@
 exports.addTEst = function(value1,value2){
     return value1+value2;
 }
+
+function calcDolar(v){
+    v1= parseInt(v);
+    return ("El valor en quetzales es: " + (v*7.8) +" Q.");
+}
+
+
